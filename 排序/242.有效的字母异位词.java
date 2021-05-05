a168bb3f77fa037c8a2e8486bf93fc3cdb09b7a9
JasonCeng <489281085@qq.com>
2021-05-05 23:22:48 +0800

    'update'

---
+++ 排序/242.有效的字母异位词.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] cs = s.toCharArray();
+        char[] ct = t.toCharArray();
+        Arrays.sort(cs);
+        Arrays.sort(ct);
+        return new String(cs).equals(new String(ct));
+    }
+}
